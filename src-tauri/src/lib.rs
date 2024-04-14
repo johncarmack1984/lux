8a319fcb461fa5f2e93fad53bdb142fcfdec4d67
John Carmack <johncarmack@me.com>
2024-04-13 17:39:08 -0700

    Remove commented out code in lib.rs

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -15,13 +15,7 @@ use channels::LuxChannels;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    // #[cfg(debug_assertions)] // only enable instrumentation in development builds
-    // let devtools = tauri_plugin_devtools::init();
-
     let builder = tauri::Builder::default();
-
-    // #[cfg(debug_assertions)]
-    // let builder = builder.plugin(devtools);
 
     let default_buffer = LuxBuffer::from([121, 255, 255, 0, 0, 42]);
     let default_channels = LuxChannels::default();
@@ -31,8 +25,6 @@ pub fn run() {
         .plugin(logger::logger().build())
         .manage(default_buffer)
         .manage(default_channels)
-        // .plugin(tauri_plugin_notification::init())
-        // .plugin(tauri_plugin_cli::init())
         .invoke_handler(tauri::generate_handler![
             cmd::update_channel_value,
             cmd::set_buffer,
